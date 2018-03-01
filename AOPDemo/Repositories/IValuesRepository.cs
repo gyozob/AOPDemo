992e8e30d10a519ce2d61293c34427951721333b
gyozob <gyozob@hotmail.com>
2018-03-01 14:56:40 +0200

    second commit

---
+++ AOPDemo/Repositories/IValuesRepository.cs
@@ -0,0 +1,15 @@
+﻿using AOPDemo.Attributes;
+
+namespace AOPDemo.Repositories
+{
+    public interface IValuesRepository
+    {
+        [Cache("{Id}-{good}")]
+        string Get(GetValueRequest request, int good);
+    }
+
+    public class GetValueRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ AOPDemo/Repositories/ValuesRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace AOPDemo.Repositories
+{
+    public class ValuesRepository : IValuesRepository
+    {
+        public string Get(GetValueRequest request, int good)
+        {
+            return $"value - {request.Id}";
+        }
+    }
+}
