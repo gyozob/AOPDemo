09fd88ea6602f6dd22b173ec209160f130a13477
gyozob <gyozob@hotmail.com>
2018-03-01 16:47:39 +0200

    add cache provider

---
+++ AOPDemo/Caching/ICacheProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AOPDemo.Caching
+{
+    public interface ICacheProvider
+    {
+        Task<T> GetOrSet<T>(string key, Func<Task<T>> cacheable);
+    }
+}
---
+++ AOPDemo/Caching/MemoryCacheProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace AOPDemo.Caching
+{
+    public class MemoryCacheProvider : ICacheProvider
+    {
+        private readonly ConcurrentDictionary<string, Task<object>> _cache;
+        public MemoryCacheProvider()
+        {
+            _cache = new ConcurrentDictionary<string, Task<object>>();
+        }
+        public async Task<T> GetOrSet<T>(string key, Func<Task<T>> cacheable)
+        {
+            return (T)await _cache.AddOrUpdate(key,
+                async (k) =>
+                {
+                    return await cacheable();
+                },
+                async (k, r) =>
+                {
+                    return await cacheable();
+                }
+                );
+        }
+    }
+}
